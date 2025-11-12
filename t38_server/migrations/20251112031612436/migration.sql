8c0805f156b745071c79c02a6b56010faa4ff44a
devabacus <32705417+devabacus@users.noreply.github.com>
2025-11-12 08:30:46 +0500

    1

---
+++ t38_server/migrations/20251112031612436/migration.sql
@@ -0,0 +1,29 @@
+BEGIN;
+
+
+--
+-- MIGRATION VERSION FOR t38
+--
+INSERT INTO "serverpod_migrations" ("module", "version", "timestamp")
+    VALUES ('t38', '20251112031612436', now())
+    ON CONFLICT ("module")
+    DO UPDATE SET "version" = '20251112031612436', "timestamp" = now();
+
+--
+-- MIGRATION VERSION FOR serverpod
+--
+INSERT INTO "serverpod_migrations" ("module", "version", "timestamp")
+    VALUES ('serverpod', '20240516151843329', now())
+    ON CONFLICT ("module")
+    DO UPDATE SET "version" = '20240516151843329', "timestamp" = now();
+
+--
+-- MIGRATION VERSION FOR serverpod_auth
+--
+INSERT INTO "serverpod_migrations" ("module", "version", "timestamp")
+    VALUES ('serverpod_auth', '20240520102713718', now())
+    ON CONFLICT ("module")
+    DO UPDATE SET "version" = '20240520102713718', "timestamp" = now();
+
+
+COMMIT;
